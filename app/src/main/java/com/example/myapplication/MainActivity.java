7e4f24e8bad374ad82d461e000d01ed152358efa
liuliang <liuliang@tango.cn>
2022-07-29 13:53:12 +0800

    feat commit 2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("test","feat_1 commit 22");
         Log.i("test","master 1111");
     }
 }
